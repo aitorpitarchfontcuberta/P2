3829408a52264c9a039d5f1fafa04c1e753674b0
bernat.garcia.bahi <bernatgb3@gmail.com>
2026-04-17 08:17:17 +0200

    final

--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -11,10 +11,10 @@ set -o pipefail
 # Write here the name and path of your program and database
 
 #Aitor
-DIR_P2=/mnt/c/Users/Aitor/Desktop/PAV/P2
+#DIR_P2=/mnt/c/Users/Aitor/Desktop/PAV/P2
 
 #Bernat
-#DIR_P2=$HOME/PAV/P2
+DIR_P2=$HOME/PAV/P2
 
 DB=$DIR_P2/db.v4
 CMD="$DIR_P2/bin/vad -0 $alpha0"
