c4f5b046f11fdd232c438a79a874256616343a9d
bernat.garcia.bahi <bernatgb3@gmail.com>
2026-04-20 10:37:34 +0200

    resultat dinal 1.0433

--- scripts/pinga.sh
+++ scripts/pinga.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-for alpha0 in $(seq 0.5 0.01 1.5); do
+
+for alpha0 in $(seq 1.034 0.0005 1.042); do
     echo -ne "alpha0 = $alpha0:\t"
     scripts/run_vad.sh $alpha0 | grep TOTAL
-done | sort -t: -k 3n
+done | sort -t: -k 3nr | head -20
--- scripts/pinga_fine.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Búsqueda fina de alpha0 con pasos de 0.002 entre 0.99 y 1.10
-
-for alpha0 in $(seq 0.99 0.002 1.10); do
-    echo -ne "alpha0 = $alpha0:\t"
-    scripts/run_vad.sh $alpha0 | grep TOTAL
-done | sort -t: -k 3nr | head -20
