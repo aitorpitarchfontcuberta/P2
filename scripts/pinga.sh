9c8bbb7d7d84eb49597f47de3c14b5d6c66000b3
bernat.garcia.bahi <bernatgb3@gmail.com>
2026-04-20 10:40:02 +0200

    resultat dinal 1.0435

--- scripts/pinga.sh
+++ scripts/pinga.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 
-for alpha0 in $(seq 1.034 0.0005 1.042); do
+for alpha0 in $(seq 1.034 0.0005 1.045); do
     echo -ne "alpha0 = $alpha0:\t"
     scripts/run_vad.sh $alpha0 | grep TOTAL
 done | sort -t: -k 3nr | head -20
